546f7d0a5b8365e35acd0f2a9a47cac19e139e6e
Joaquin Bebekian <joacobebekian115@gmail.com>
2024-04-03 08:12:57 -0300

    creamos el archivo

---
+++ ejemplo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>letra</h1>
+    <?php   
+        echo 'No se si hice bien, pero llame';
+        echo 'No me gusto lo que escuche';
+        ?>
+</body>
+</html>
